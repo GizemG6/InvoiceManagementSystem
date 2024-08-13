076ee88b0fcd0b566367cd835d3d87827980eb03
cang8 <0.gizemgunes@gmail.com>
2024-08-13 23:12:54 +0300

    add-repository

---
+++ FaturaYönetimSistemi/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using FaturaYönetimSistemi.Models.DB;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FaturaYönetimSistemi.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Remove(T entity);
+    }
+}
---
+++ FaturaYönetimSistemi/Repositories/Repository.cs
@@ -0,0 +1,48 @@
+﻿using FaturaYönetimSistemi.Models.DB;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FaturaYönetimSistemi.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly Context _context;
+        private readonly DbSet<T> _entities;
+
+        public Repository(Context context)
+        {
+            _context = context;
+            _entities = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _entities.ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _entities.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _entities.Where(predicate).ToListAsync();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await _entities.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _entities.Update(entity);
+        }
+
+        public void Remove(T entity)
+        {
+            _entities.Remove(entity);
+        }
+    }
+}

9340636856755a4f91c7e433b39d509117b27535
cang8 <0.gizemgunes@gmail.com>
2024-08-15 00:15:49 +0300

    add-userrepository

---
+++ FaturaYönetimSistemi/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using FaturaYönetimSistemi.Models.Entities;
+
+namespace FaturaYönetimSistemi.Repositories.UserRepository
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<User> GetUserWithDetailsAsync(int id);
+    }
+}
---
+++ FaturaYönetimSistemi/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using FaturaYönetimSistemi.Models.DB;
+using FaturaYönetimSistemi.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FaturaYönetimSistemi.Repositories.UserRepository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(Context context) : base(context) { }
+
+        public async Task<User> GetUserWithDetailsAsync(int id)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.UserId == id);
+        }
+    }
+}

143a892a65525de0e756544d5ca69fbf048aa158
cang8 <0.gizemgunes@gmail.com>
2024-11-01 14:44:50 +0300

    added-messageController

---
+++ InvoiceManagementSystem/Controllers/MessageController.cs
@@ -0,0 +1,22 @@
+﻿using InvoiceManagementSystem.Models.Entities;
+using InvoiceManagementSystem.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvoiceManagementSystem.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IService<Message> _messageService;
+
+        public MessageController(IService<Message> messageService)
+        {
+            _messageService = messageService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var messages = await _messageService.GetAllAsync();
+            return View(messages);
+        }
+    }
+}

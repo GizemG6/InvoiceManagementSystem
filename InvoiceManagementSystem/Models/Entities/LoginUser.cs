6f025077906d7857df1b4d20126a95b945914e24
cang8 <0.gizemgunes@gmail.com>
2024-11-04 00:36:06 +0300

    deleted-loginUser

--- InvoiceManagementSystem/Models/Entities/LoginUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InvoiceManagementSystem.Models.Entities
-{
-    public class LoginUser
-    {
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}

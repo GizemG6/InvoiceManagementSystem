2666ad7b6f9888f2259d60b1cbcbd48fde5b4c63
cang8 <0.gizemgunes@gmail.com>
2024-11-14 23:29:37 +0300

    added-SendMessageToAdminMethod

--- InvoiceManagementSystem/Controllers/MessageController.cs
+++ InvoiceManagementSystem/Controllers/MessageController.cs
@@ -1,16 +1,20 @@
-﻿using InvoiceManagementSystem.Models.Entities;
+﻿using InvoiceManagementSystem.Models.Context;
+using InvoiceManagementSystem.Models.Entities;
 using InvoiceManagementSystem.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceManagementSystem.Controllers
 {
     public class MessageController : Controller
     {
         private readonly IService<Message> _messageService;
+        private readonly ApplicationDbContext _context;
 
-        public MessageController(IService<Message> messageService)
+        public MessageController(IService<Message> messageService, ApplicationDbContext context)
         {
             _messageService = messageService;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
@@ -18,5 +22,32 @@ namespace InvoiceManagementSystem.Controllers
             var messages = await _messageService.GetAllAsync();
             return View(messages);
         }
+        public async Task<IActionResult> SendMessageToAdmin(int RecipientId, string Title, string Comment)
+        {
+            var userId = HttpContext.Session.GetInt32("UserId");
+            if (userId == null)
+            {
+                TempData["ErrorMessage"] = "Giriş yapmadan mesaj gönderemezsiniz.";
+                return RedirectToAction("UserIndex", "User");
+            }
+
+            var message = new Message
+            {
+                SenderId = userId.Value,
+                RecipientId = RecipientId, 
+                Title = Title,
+                Comment = Comment,
+                Status = false,
+                SendDate = DateTime.Now,
+                IsDelete = false
+            };
+
+            _context.Messages.Add(message);
+            await _context.SaveChangesAsync();
+
+            TempData["SuccessMessage"] = "Mesaj başarıyla seçilen admin'e gönderildi.";
+            return RedirectToAction("UserIndex", "User");
+        }
+
     }
 }

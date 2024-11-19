fea5910e3f3e2ada97b6eb91693df69149d10ec4
cang8 <0.gizemgunes@gmail.com>
2024-11-19 17:41:44 +0300

    added-mongoDbService

--- PaymentService/Program.cs
+++ PaymentService/Program.cs
@@ -8,6 +8,7 @@ namespace PaymentService
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            //builder.Services.AddSingleton<MongoDbService>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ PaymentService/Services/MongoDbService.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Driver;
+using PaymentService.Models;
+
+namespace PaymentService.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoCollection<Payment> _paymentsCollection;
+
+        public MongoDbService(IConfiguration configuration)
+        {
+            var mongoClient = new MongoClient(configuration.GetConnectionString("MongoDbSettings:mongodb+srv://0gizemgunes:XqQFObRPmJxE0uOg@paymentserviceforinvoic.oxd2e.mongodb.net/?retryWrites=true&w=majority&appName=PaymentServiceForInvoiceSystem"));
+            var database = mongoClient.GetDatabase(configuration["MongoDbSettings:PaymentServiceForInvoiceSystem"]);
+            _paymentsCollection = database.GetCollection<Payment>(configuration["MongoDbSettings:Payments"]);
+        }
+
+        public async Task SavePaymentAsync(Payment payment)
+        {
+            await _paymentsCollection.InsertOneAsync(payment);
+        }
+    }
+}

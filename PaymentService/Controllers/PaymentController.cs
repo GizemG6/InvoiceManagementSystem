08ddd0744e3666e2dc9d390008b1371273f61788
cang8 <0.gizemgunes@gmail.com>
2024-11-19 16:16:22 +0300

    added-PaymentService

---
+++ PaymentService/Controllers/PaymentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace PaymentService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        
+    }
+}
